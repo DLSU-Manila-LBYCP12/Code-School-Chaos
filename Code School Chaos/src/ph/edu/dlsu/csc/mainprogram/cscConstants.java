6e5bacd03909de2cdb371c20a42cca14ee118f0c
ChristophJohnEric <ChristophJohnEric@192.168.1.8>
2017-08-17 19:50:03 +0800

    Constants, for easy changes

---
+++ Code School Chaos/src/ph/edu/dlsu/csc/mainprogram/cscConstants.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ph.edu.dlsu.csc.mainprogram;
+
+import java.awt.Color;
+import java.awt.Font;
+
+/**
+ *
+ * @author ChristophJohnEric
+ */
+public interface cscConstants {
+    /* Application width and height of the Game. */
+    public static final int APPLICATION_WIDTH = 400;
+    
+    public static final int APPLICATION_HEIGHT = 700;
+    
+    /* Sizes of the Buttons */
+    public static final int BUTTON_WIDTH = 50;
+    
+    public static final int BUTTON_HEIGHT = 150;
+    
+    public static final Font BFONT = new Font("Courier",0,24);
+    
+    public static final Color BCLICKED = Color.DARK_GRAY;
+}
