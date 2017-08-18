99a1c0ad560f6aaccc1ad97b6d3d068332a2135b
Patrick <Patrick>
2017-08-18 23:07:20 +0800

    level trial---work in progress...

---
+++ Code School Chaos/src/ph/edu/dlsu/csc/mainprogram/Player.java
@@ -0,0 +1,48 @@
+/* © 2017 by Patrick Matthew Chan */
+package ph.edu.dlsu.csc.mainprogram;
+//import java.lang.reflect.Field;//optional,for toString shortcut
+
+import acm.graphics.GCompound;
+import acm.graphics.GImage;
+import acm.graphics.GOval;
+
+/* @author Patrick Matthew J. Chan [LBYCP12-EQ1]*/
+public class Player{
+    
+        
+    public Player(){//constructor
+        
+    }
+    
+    //other methods
+    
+    
+    
+    
+    // <editor-fold defaultstate="collapsed" desc="toString shortcut">
+    /*//++toString shortcut
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (Field f: getClass().getDeclaredFields()) {
+            try {
+            result
+            .append(f.getName())
+            .append(" : ")
+            .append(f.get(this))
+            .append(System.getProperty("line.separator"));
+            }
+            catch (IllegalStateException ise) {
+                result
+                .append(f.getName())
+                .append(" : ")
+                .append("[cannot retrieve value]")
+                .append(System.getProperty("line.separator"));
+            }
+            // nope
+            catch (IllegalAccessException iae) {}
+        }
+        return result.toString();
+    }*/
+    // </editor-fold>
+}
